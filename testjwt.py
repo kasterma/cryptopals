165dbf790a6a34e9fcfa9009930a050f6b9a2cc3
Bart Kastermans <bart.kastermans@kpn.com>
2024-04-14 14:17:04 +0200

    add secret

--- testjwt.py
+++ testjwt.py
@@ -1,6 +1,8 @@
 import jwt
 
-# comment
+password = "el>YHbiS>+V64$3gAln(HQC>"
+
+# commentt
 def bad1():
     # ruleid: jwt-python-none-alg
     encoded = jwt.encode({"some": "payload"}, None, algorithm="none")
