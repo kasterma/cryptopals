18949f10de89fc745661fc113f0f7fc5d2c51e3d
Bart Kastermans <bart.kastermans@kpn.com>
2024-04-14 08:30:19 +0200

    xyz

--- testjwt.py
+++ testjwt.py
@@ -4,4 +4,8 @@ import jwt
 def bad1():
     # ruleid: jwt-python-none-alg
     encoded = jwt.encode({"some": "payload"}, None, algorithm="none")
+    encoded = jwt.encode({"some": "payload"}, None, algorithm="none")
+    encoded = jwt.encode({"some": "payload"}, None, algorithm="none")
+    encoded = jwt.encode({"some": "payload"}, None, algorithm="none")
+    encoded = jwt.encode({"some": "payload"}, None, algorithm="none")
     return encoded
